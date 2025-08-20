a551ab58c43a1a00a00399881288e55594a6089c
sarbondev <suhrobrahmatullayev973132@gmail.com>
2025-08-20 16:01:12 +0500

    something

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   age: { type: Number, required: true },
 });
-
+``;
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   const salt = await bcrypt.genSalt(10);
