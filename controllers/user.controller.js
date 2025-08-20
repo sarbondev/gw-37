a94a292de0071d6f3dfaa31ece840e54da2eca42
sarbondev <suhrobrahmatullayev973132@gmail.com>
2025-08-20 16:36:07 +0500

    test

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -40,11 +40,9 @@ export const createUser = async (req, res) => {
 
     await newUser.save();
 
-    res.status(201).json({
-      status: "Foydalanuvchi yaratildi",
-      data: {
-        user: newUser,
-      },
+    return res.status(201).json({
+      status: "Foydalanuvchi muvaffaqiytali yaratildi",
+      newUser,
     });
   } catch (err) {
     res.status(400).json({
@@ -56,20 +54,49 @@ export const createUser = async (req, res) => {
 
 export const updateUser = async (req, res) => {
   try {
-    const user = await User.findByIdAndUpdate({ _id: req.params.id });
+    const { id } = req.params;
+    const { fullName, password, phoneNumber } = req.body;
 
-    if (!user) {
+    if (!fullName || !password || !phoneNumber) {
+      return res.status(409).json({
+        message: "Hamma maydonlar to'ldiriishi shart!",
+        status: "failed",
+      });
+    }
+
+    const isUserExist = await User.findOne(phoneNumber);
+
+    if (!isUserExist) {
       return res.status(404).json({
         status: "error",
         message: "Foydalanuvchi topilmadi",
       });
     }
 
-    await user.updateOne(req.body);
+    const isPasswordCorrect = await User.isPasswordCorrect(
+      password,
+      isUserExist.password
+    );
 
-    res.status(200).json({
+    if (!isPasswordCorrect) {
+      return res.status(404).json({
+        status: "error",
+        message: "Parol yoki telefon raqam noto'g'ri",
+      });
+    }
+
+    const updatedUser = {
+      fullName,
+      password,
+      phoneNumber,
+    };
+
+    const user = await User.findByIdAndUpdate(id, updatedUser, { new: true });
+
+    return res.status(200).json({
       status: "success",
       message: "Foydalanuvchi yangilandi",
+      user,
     });
   } catch (err) {
     res.status(500).json({
@@ -78,9 +105,11 @@ export const updateUser = async (req, res) => {
     });
   }
 };
+
 export const deleteUser = async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete({ _id: req.params.id });
+    const { id } = req.params;
+    const user = await User.findByIdAndDelete(id);
 
     if (!user) {
       return res.status(404).json({
